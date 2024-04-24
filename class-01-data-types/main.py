ce45259f8b1ebc398a368dad171d8a48826c25fd
Eric Batista <batistaeric62@gmail.com>
2024-04-24 20:31:00 -0300

    add structural types examples

--- class-01-data-types/main.py
+++ class-01-data-types/main.py
@@ -1,19 +1,76 @@
-milk = "Leite"
-volume = 2.5
+# Primitive Types
 
-print(type(milk))
-print(type(volume))
+# milk = "Leite"
+# volume = 2.5
 
-preparation1 = volume + 1
-preparation2 = milk + str(volume)
+# print(type(milk))
+# print(type(volume))
 
-print(preparation1)
-print(preparation2)
+# preparation1 = volume + 1
+# preparation2 = milk + str(volume)
 
-life_cat = str(volume + 5)
+# print(preparation1)
+# print(preparation2)
 
-print("O gato bebe " + milk )
-print("e tem " + life_cat + " vidas")
+# life_cat = str(volume + 5)
 
-print("O gato bebe " + milk + " e tem " + life_cat + " vidas")
-print(f"O gato bebe {milk} e tem {life_cat} vidas")
+# print("O gato bebe " + milk )
+# print("e tem " + life_cat + " vidas")
+
+# print("O gato bebe " + milk + " e tem " + life_cat + " vidas")
+# print(f"O gato bebe {milk} e tem {life_cat} vidas")
+
+name = "Eric"
+age = 19
+is_admin = True #boolean True or False
+
+print(is_admin)
+print(type(is_admin))
+
+# Structural Types (Collections)
+
+animes = ["Saint Seiya", "Dragon Ball", "Pokemon", "Nanatsu no Taizai", "Spy X Family"]
+            # =>  0             1           2               3                 4
+            # <= -5            -4          -3              -2                -1
+
+print(animes)
+print(type(animes))
+
+print(animes[3])
+print(animes[0])
+print(animes[-1])
+
+list = ["A", 123, True, ["B", "C"]]
+
+print(list[0])
+print(list[3])
+print(list[-1])
+print(list[-1][1])
+
+list[2] = False
+list[-1][0] = "D"
+print(list)
+print(len(list)) #length
+
+# tuple
+tuple = ("A", 123, True)
+print(type(tuple))
+print(tuple[1])
+
+# tuple[1] = 456 # error: tuple is immutable
+
+print(tuple)
+
+# dict
+
+dict = {
+    "name": "Eric",
+    "age": "19",
+    "is_admin": True
+}
+print(dict)
+print(type(dict))
+
+print(dict["name"])
+print(dict["age"])
+print(dict["is_admin"])
