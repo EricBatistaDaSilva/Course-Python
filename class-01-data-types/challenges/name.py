081b6c5dae03ee728c124b28f35ee14dd57e3ebd
Eric Batista <batistaeric62@gmail.com>
2024-04-25 19:41:02 -0300

    add challenge name

---
+++ class-01-data-types/challenges/name.py
@@ -0,0 +1,7 @@
+# Capture o nome do usuário via "input"
+# Atribua o nome a uma variável
+# Imprima na tela um mensagem de boas vindas com o nome
+
+name = input("Informe seu nome: ")
+
+print("Boa noite, " + name + " seja bem-vindo ao curso de Python!")
